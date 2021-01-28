98429436b97a266119b7bd349e557abf8299690a
DarkShadow77 <dadeshina4@gmail.com>
2021-01-28 21:05:57 +0100

    commit

---
+++ lib/LoginPage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'Header.dart';
+import 'InputWrapper.dart';
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+            Colors.cyan[500],
+            Colors.cyan[300],
+            Colors.cyan[400]
+          ]),
+        ),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Header(),
+            Expanded(child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+              ),
+              child: InputWrapper(),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
